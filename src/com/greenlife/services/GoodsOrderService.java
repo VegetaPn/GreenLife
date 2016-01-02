ca18595d103bc5b9367af1234bf8300ab1d2c1ca
xyk0058 <1139834544@qq.com>
2016-01-02 16:22:01 +0800

    xyk

--- src/com/greenlife/services/GoodsOrderService.java
+++ src/com/greenlife/services/GoodsOrderService.java
@@ -5,7 +5,9 @@ import com.greenlife.model.GoodsOrder;
 
 public class GoodsOrderService {
 	public static void cancleOrder(GoodsOrder order){
-		order.setOrderState(20);
+		order.setOrderState(19);
 		GoodsOrderDao.updateGoodsOrder(order);
 	}
+	
+	
 }
